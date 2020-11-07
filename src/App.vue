372eeaf44c04395819afec83c9e5a1b4e4f5e243
LYuanshen <707978562@qq.com>
2020-11-08 00:54:49 +0800

    2020-11-8

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <p style="font-size: 20px;color: #ffade8">刘佩奇与陈猪猪的甜蜜爱爱</p>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
